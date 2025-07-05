c128bfe2340790dc9c1089d7ed4669e75bdc0ecd
Sunwoo Kim <ssunwoo0110@gmail.com>
2025-07-05 20:41:16 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.35 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -0,0 +1,26 @@
+from collections import deque
+import math
+
+def solution(players, m, k):
+    answer = 0
+    
+    queue = deque()
+        
+    for i in range(24):
+        while queue:
+            if i-queue[0] < k:
+                break
+                
+            queue.popleft()
+            
+        totalServer = len(queue)
+        need = players[i]//m
+        
+        print(totalServer, need)
+        
+        if need > totalServer:
+            for j in range((need-totalServer)):
+                queue.append(i)
+                answer += 1
+    
+    return answer
