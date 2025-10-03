34bbbe38766f1e5207769ea26abe519ece86c9fa
Sunwoo Kim <ssunwoo0110@gmail.com>
2025-10-03 19:43:58 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 10.30 ms, Memory: 9.82 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -2,37 +2,39 @@ from collections import defaultdict
 
 def solution(friends, gifts):
     answer = 0
+    
+    idx_map = defaultdict(int)
     n = len(friends)
     
-    friend_index = defaultdict(int)
-    gift_arr = [[0 for _ in range(n)] for _ in range(n)] ## 주고 받은 선물 기록
-    gift_rate = [0 for _ in range(n)] ## 선물 지수
-    nxt_gift = [0 for _ in range(n)] ## 다음달 받을 선물
+    for idx, f in enumerate(friends):
+        idx_map[f] = idx
+        
+    gift_num = [[0 for _ in range(n)] for _ in range(n)]
+    gift_rate = [0 for _ in range(n)]
+    get_gift = [0 for _ in range(n)] ## 다음 달에 받을 선물 개수
     
-    for i in range(n):
-        friend_index[friends[i]] = i
-        
     for g in gifts:
-        sender, receiver = g.split(" ")
-        sender_idx, receiver_idx = friend_index[sender], friend_index[receiver]
+        a, b = g.split(" ")
+        gift_num[idx_map[a]][idx_map[b]] += 1 ## a가 b에게 선물 준 기록
         
-        gift_arr[sender_idx][receiver_idx] += 1
-        gift_rate[sender_idx] += 1
-        gift_rate[receiver_idx] -= 1
-        
+        ## 선물 지수
+        gift_rate[idx_map[a]] += 1 
+        gift_rate[idx_map[b]] -= 1
+    
     for i in range(n):
         for j in range(i+1, n):
-            if gift_arr[i][j] > gift_arr[j][i]:
-                nxt_gift[i] += 1
-            elif gift_arr[i][j] < gift_arr[j][i]:
-                nxt_gift[j] += 1
-            else:
+            
+            if (gift_num[i][j] == 0 and gift_num[j][i] == 0) or (gift_num[i][j] != 0 and gift_num[j][i] != 0 and gift_num[i][j] == gift_num[j][i]):
                 if gift_rate[i] > gift_rate[j]:
-                    nxt_gift[i] += 1
+                    get_gift[i] += 1
                 elif gift_rate[i] < gift_rate[j]:
-                    nxt_gift[j] += 1
-                else:
-                    pass
-    
-    answer = max(nxt_gift)
+                    get_gift[j] += 1
+                    
+            elif gift_num[i][j] != 0 or gift_num[j][i] != 0:
+                if gift_num[i][j] > gift_num[j][i]:
+                    get_gift[i] += 1
+                elif gift_num[i][j] < gift_num[j][i]:
+                    get_gift[j] += 1
+                    
+    answer = max(get_gift)
     return answer
