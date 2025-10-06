d96c92841396bc93e131fc443326753ebf1005aa
Sunwoo Kim <ssunwoo0110@gmail.com>
2025-10-06 23:12:09 +0900

    [level 2] Title: 순위 검색, Time: 1085.89 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.py
@@ -1,49 +1,45 @@
 def solution(info, query):
     answer = []
     
-    # 매핑
-    L_MAP = {'cpp':0, 'java':1, 'python':2}
-    P_MAP = {'backend':0, 'frontend':1}
-    C_MAP = {'junior':0, 'senior':1}
-    F_MAP = {'chicken':0, 'pizza':1}
-
-    MAX_SCORE = 100000
+    ## 누적합
+    info_sum = [[[[[0 for _ in range(100001)] for _ in range(2)] for _ in range(2)] for _ in range(2)] for _ in range(3)]
     
-    counts = [[[[[0]*(MAX_SCORE+1) for _ in range(2)] for _ in range(2)] for _ in range(2)] for _ in range(3)]
-
-
-    ## info 전처리: 24조합만 점수 카운팅
-    for row in info:
-        l, p, c, f, s = row.split()
-        s = int(s)
-        counts[L_MAP[l]][P_MAP[p]][C_MAP[c]][F_MAP[f]][s] += 1
-
-    ## 누적합: arr[x] = 점수가 x 이상인 인원 수
+    idx_map = {'cpp': 0, 'java': 1, 'python': 2, 'backend': 0, 'frontend': 1, 
+               'junior': 0, 'senior': 1, 'chicken': 0, 'pizza': 1}
+    
+    for ifo in info:
+        a, b, c, d, score = ifo.split(" ")
+        score = int(score)
+        
+        info_sum[idx_map[a]][idx_map[b]][idx_map[c]][idx_map[d]][score] += 1
+            
     for li in range(3):
         for pi in range(2):
             for ci in range(2):
                 for fi in range(2):
-                    arr = counts[li][pi][ci][fi] ## 0~100,000
+                    arr = info_sum[li][pi][ci][fi] ## 0 ~ 100,000
                     running = 0
-                    for sc in range(MAX_SCORE, -1, -1):
+                    for sc in range(100000, -1, -1):
                         running += arr[sc]
                         arr[sc] = running
-
-    ## 쿼리 처리: 각 차원별 후보 인덱스 만들고 해당 셀들의 arr[score] 합산
-    for q in query:
-        q = q.replace("and ", "").split()
-        lq, pq, cq, fq, X = q[0], q[1], q[2], q[3], int(q[4])
-
-        Ls = range(3) if lq == '-' else [L_MAP[lq]]
-        Ps = range(2) if pq == '-' else [P_MAP[pq]]
-        Cs = range(2) if cq == '-' else [C_MAP[cq]]
-        Fs = range(2) if fq == '-' else [F_MAP[fq]]
-
+    
+    for qu in query:
+        a, b, c, d_score = qu.split(" and ")
+        d, score = d_score.split(" ")
+        score = int(score)
+        
+        a = range(3) if a == '-' else [idx_map[a]]
+        b = range(2) if b == '-' else [idx_map[b]]
+        c = range(2) if c == '-' else [idx_map[c]]
+        d = range(2) if d == '-' else [idx_map[d]]
+        
         total = 0
-        for li in Ls:
-            for pi in Ps:
-                for ci in Cs:
-                    for fi in Fs:
-                        total += counts[li][pi][ci][fi][X]
+        for li in a:
+            for pi in b:
+                for ci in c:
+                    for fi in d:
+                        total += info_sum[li][pi][ci][fi][score]
         answer.append(total)
+        
+    
     return answer
