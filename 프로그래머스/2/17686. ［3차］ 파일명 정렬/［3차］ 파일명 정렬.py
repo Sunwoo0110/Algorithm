9cf2b727bbf83feb34ddcfe1c61b0897ab4eba84
Sunwoo Kim <ssunwoo0110@gmail.com>
2025-10-08 21:51:33 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 5.35 ms, Memory: 9.86 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,29 +1,32 @@
 def solution(files):
     answer = []
     
-    file_arr = []
+    sort_files = []
     
-    for f_idx, file in enumerate(files):
-        idx = 0
-        head = ""
-        number = ""
-        while idx < len(file):
-            if file[idx] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-                break
-            
-            head += file[idx]
-            idx += 1
-            
-        while idx < len(file):
-            if file[idx] not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-                break
-            
-            number += file[idx]
-            idx += 1 
+    for idx, file in enumerate(files):
+        head, number = "", ""
+        i = 0
+        l = len(file)
         
-        file_arr.append((head.upper(), int(number), f_idx))
+        while i < l:
+            if file[i].isdigit():
+                break 
+            head += file[i]
+            i += 1
+        
+        while i < l: 
+            if not file[i].isdigit():
+                break
+            
+            number += file[i]
+            i += 1
+        
+        head = head.upper()
+        sort_files.append((head, int(number), idx))
+        
+    sort_files.sort()
     
-    file_arr.sort()
-    answer = [files[i[2]] for i in file_arr]
+    for h, n, i in sort_files:
+        answer.append(files[i])
         
     return answer
