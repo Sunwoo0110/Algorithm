3d611834188425bdad0107799b61bd82789eb1b4
Sunwoo Kim <ssunwoo0110@gmail.com>
2025-11-27 19:29:43 +0900

    [level 2] Title: 오픈채팅방, Time: 105.08 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/README.md
+++ 프로그래머스/2/42888. 오픈채팅방/README.md
@@ -1,10 +1,10 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888) 
 
 ### 성능 요약
 
-메모리: 35.8 MB, 시간: 143.96 ms
+메모리: 40.5 MB, 시간: 105.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 03일 22:51:37
+2025년 11월 27일 19:29:41
 
 ### 문제 설명
 
--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -3,22 +3,25 @@ from collections import defaultdict
 def solution(record):
     answer = []
     
-    idName = defaultdict(str)
+    uid_nickname_dict = defaultdict(str)
     
     for rec in record:
-        recArr = rec.split(" ")
-        id = recArr[1]
-        
-        if recArr[0] == "Enter" or recArr[0] == "Change":
-            idName[id] = recArr[2]
+        rec_split = rec.split(" ")
+        typ, uid = rec_split[0], rec_split[1]
+        if typ == "Enter" or typ == "Change":
+            uid_nickname_dict[uid] = rec_split[2]
             
     for rec in record:
-        recArr = rec.split(" ")
-        id = recArr[1]
+        rec_split = rec.split(" ")
+        typ, uid = rec_split[0], rec_split[1]
         
-        if recArr[0] == "Enter": 
-            answer.append(idName[id] + "님이 들어왔습니다.")
-        elif recArr[0] == "Leave": 
-            answer.append(idName[id] + "님이 나갔습니다.")
+        result = ""
+        
+        if typ == "Enter":
+            result = f"{uid_nickname_dict[uid]}님이 들어왔습니다."
+            answer.append(result)
+        elif typ == "Leave":
+            result = f"{uid_nickname_dict[uid]}님이 나갔습니다."
+            answer.append(result)
     
     return answer
