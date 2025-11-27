1ac686e569d6d8fb9acd1a13ae70f2c2d76287ac
Sunwoo Kim <ssunwoo0110@gmail.com>
2025-11-27 18:59:15 +0900

    [level 2] Title: 전화번호 목록, Time: 432.43 ms, Memory: 40.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,11 @@
+def solution(phone_book):
+    answer = True
+    
+    phone_set = set(phone_book)
+    
+    for phone in phone_book:
+        for i in range(1, len(phone)):
+            if phone[:i] in phone_set:
+                return False
+        
+    return answer
