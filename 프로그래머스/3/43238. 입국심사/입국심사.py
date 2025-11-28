66a16cb70822b303c0a05ae167b0f86a8c33e981
Sunwoo Kim <ssunwoo0110@gmail.com>
2025-11-28 18:34:58 +0900

    [level 3] Title: 입국심사, Time: 481.36 ms, Memory: 13.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,18 +1,24 @@
-def solution(n, times):
-    answer = 0
+def solution(n, times):    
+    left, right = 1, max(times) * n
+    answer = right
     
-    left = 1
-    right = min(times)*n
-    
-    while left < right:
-        mid = (left+right) // 2 ## 최솟값 후보
-        people = sum(mid//time for time in times) ## 심사 받을 수 있는 수
+    while left <= right:
+        mid = (left+right) // 2
         
+        ## mid 시간 동안 처리 가능한 사람 수
+        people = 0
+        for t in times:
+            people += mid // t
+            
+            if people >= n:
+                break
+
         if people >= n:
-            right = mid
-            
+            ## mid 시간으로도 n명을 다 처리 가능
+            answer = mid
+            right = mid-1
         else:
+            ## mid 시간으로 부족
             left = mid+1
-            
-    answer = left
+    
     return answer
