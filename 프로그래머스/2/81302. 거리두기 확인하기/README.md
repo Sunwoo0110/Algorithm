20a8a1e29e4810b3106754cf121b59de81011caa
Sunwoo Kim <ssunwoo0110@gmail.com>
2025-11-29 17:51:49 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.10 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302?language=python3) 
 
 ### 성능 요약
 
-메모리: 7.88 MB, 시간: 0.17 ms
+메모리: 9.32 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 01일 23:12:45
+2025년 11월 29일 17:51:46
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,43 +1,47 @@
+from collections import deque
+
+def check(place): 
+    for i in range(5):
+        for j in range(5):
+            if place[i][j] != 'P':
+                continue
+            
+            ## (i,j)에서 BFS 시작
+            queue = deque()
+            queue.append((i, j, 0))
+            visited = [[False]*5 for _ in range(5)]
+            visited[i][j] = True
+            
+            while queue:
+                x, y, dist = queue.popleft()
+                
+                ## 거리 2까지만 확인
+                if dist >= 2:
+                    continue
+                
+                for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    nx, ny = x+dx, y+dy
+                    if not (0 <= nx < 5 and 0 <= ny < 5):
+                        continue
+                    if visited[nx][ny]:
+                        continue
+                    if place[nx][ny] == 'X':  ## 파티션이면 더 안감
+                        continue
+                    
+                    ## 빈 자리(O)이면 계속 탐색
+                    if place[nx][ny] == 'O':
+                        visited[nx][ny] = True
+                        queue.append((nx, ny, dist+1))
+                    
+                    ## 응시자(P)를 찾음 -> 거리두기 위반
+                    if place[nx][ny] == 'P':
+                        return 0
+    return 1
+
 def solution(places):
     answer = []
     
-    isFail = False
-    
     for place in places:
-        isFail = False
-        
-        for i in range(5):
-            for j in range(5):
-                if place[i][j] == 'P':
-                    for mx, my in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
-                        if i+mx < 5 and i+mx >= 0 and j+my < 5 and j+my >= 0 and place[i+mx][j+my] == 'P' and isFail == False:
-                            answer.append(0)
-                            isFail = True
-                            break
-                    
-                    for mx, my in [[-1, 1], [1, 1], [1, -1], [-1, -1]]:
-                        if i+mx < 5 and i+mx >= 0 and j+my < 5 and j+my >= 0 and place[i+mx][j+my] == 'P' and isFail == False:
-                             if place[i][j+my] != 'X' or place[i+mx][j] != 'X':
-                                    answer.append(0)
-                                    isFail = True
-                                    break
-                    
-                    for mx, my in [[-2, 0], [2, 0], [0, 2], [0, -2]]:
-                        if i+mx < 5 and i+mx >= 0 and j+my < 5 and j+my >= 0 and place[i+mx][j+my] == 'P' and isFail == False:
-                            if place[i+(mx//2)][j+(my//2)] != 'X':
-                                answer.append(0)
-                                isFail = True
-                                break
-                            
-                            
-                if isFail == True:
-                    break
-            if isFail == True:
-                break
-        
-        if isFail == False:
-            answer.append(1)
-            
-                            
+        answer.append(check(place))
         
     return answer
