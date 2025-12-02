e3e7bc6793bc3a7f6d33fa2a28ad36a1e32b419d
Sunwoo Kim <ssunwoo0110@gmail.com>
2025-12-02 10:36:38 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 817.82 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -2,30 +2,28 @@ def solution(board, skill):
     answer = 0
     n, m = len(board), len(board[0])
     
-    acc = [[0 for _ in range(m+1)] for _ in range(n+1)] ## 누적합
-
+    prefix = [[0 for _ in range(m+1)] for _ in range(n+1)]
+    
     for typ, r1, c1, r2, c2, degree in skill:
         if typ == 1:
-            degree = -degree
-
-        acc[r1][c1] += degree
-        acc[r1][c2+1] -= degree
-        acc[r2+1][c1] -= degree
-        acc[r2+1][c2+1] += degree
-
-    ## 행 누적합
-    for i in range(n+1):
-        for j in range(1, m+1):
-            acc[i][j] += acc[i][j-1]
-
-    ## 열 누적합
-    for j in range(m+1):
-        for i in range(1, n+1):
-            acc[i][j] += acc[i-1][j]
-
+            degree *= -1
+            
+        prefix[r1][c1] += degree
+        prefix[r2+1][c2+1] += degree
+        prefix[r1][c2+1] -= degree
+        prefix[r2+1][c1] -= degree
+    
+    for i in range(n):
+        for j in range(1, m):
+            prefix[i][j] += prefix[i][j-1]
+    
+    for j in range(m):
+        for i in range(1, n):
+            prefix[i][j] += prefix[i-1][j]
+            
     for i in range(n):
         for j in range(m):
-            if board[i][j] + acc[i][j] > 0:
+            if board[i][j]+prefix[i][j] >= 1:
                 answer += 1
-
+    
     return answer
