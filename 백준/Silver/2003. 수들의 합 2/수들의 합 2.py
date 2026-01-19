45455f6465bc83799b0e16ff446ffc6977ca15d8
Sunwoo Kim <ssunwoo0110@gmail.com>
2026-01-19 18:58:01 +0900

    [Silver IV] Title: 수들의 합 2, Time: 56 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -8,21 +8,22 @@ def main():
     arr = list(map(int, input().split()))
     cnt = 0
     
-    left, right = 0, 0
-    total = 0
-    
-    while right < n:
+    left = 0
+    current = 0
+    cnt = 0
+
+    for right in range(n):
+        current += arr[right]
         
-        if total+arr[right] == m:
+        while current > m and left <= right:
+            current -= arr[left]
+            left += 1
+
+        if current == m:
             cnt += 1
-            total -= arr[left]
+            current -= arr[left]
             left += 1
-        elif total+arr[right] < m:
-            total += arr[right]
-            right += 1
-        else:
-            total -= arr[left]
-            left += 1
+
     print(cnt)
     
 
