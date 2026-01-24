8f5e657798141761dcb1d6d1dff88de4470f53ff
Sunwoo Kim <ssunwoo0110@gmail.com>
2026-01-24 19:07:48 +0900

    [Silver II] Title: 최소 힙, Time: 92 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,23 @@
+import sys
+import heapq
+
+def main():
+    input = sys.stdin.readline
+    
+    n = int(input())
+    heap = []
+    
+    for _ in range(n):
+        x = int(input())
+        
+        if x == 0:
+            if heap:
+                print(heapq.heappop(heap))
+            else:
+                print(0)
+        else:
+            heapq.heappush(heap, x)
+    
+
+if __name__ == "__main__":
+    main()
