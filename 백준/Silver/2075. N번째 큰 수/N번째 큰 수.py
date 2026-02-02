cec5ab31c631336367a04d2e8eb8492f15e3416b
Sunwoo Kim <ssunwoo0110@gmail.com>
2026-02-02 23:31:15 +0900

    [Silver III] Title: N번째 큰 수, Time: 456 ms, Memory: 35508 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -0,0 +1,23 @@
+import sys
+import heapq
+
+def main():
+    input = sys.stdin.readline
+    
+    n = int(input())
+    
+    heap = []
+
+    for _ in range(n):
+        row = map(int, input().split())
+        for x in row:
+            if len(heap) < n:
+                heapq.heappush(heap, x)
+            else:
+                if x > heap[0]:
+                    heapq.heapreplace(heap, x)
+
+    print(heap[0])
+            
+if __name__ == "__main__":
+    main()
