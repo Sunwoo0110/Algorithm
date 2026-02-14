768179c72c6c3c2d9d28d16fbad4f7dc44659186
Sunwoo Kim <ssunwoo0110@gmail.com>
2026-02-14 22:03:44 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 76 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,11 +1,20 @@
-num = int(input())
-DP = [1] * num
-arr = [0] * num
+import sys
+from collections import deque
 
-arr = list(map(int, input().split()))
+def main():
+    input = sys.stdin.readline
     
-for i in range(1, num):
-    for j in range(i):
-        if arr[i] > arr[j]:
-            DP[i] = max(DP[i], DP[j]+1)
-print(max(DP))
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    dp = [1 for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(i):
+            if arr[i] > arr[j]:
+                dp[i] = max(dp[i], dp[j]+1)
+    
+    print(max(dp))
+    
+if __name__ == "__main__":
+    main()
