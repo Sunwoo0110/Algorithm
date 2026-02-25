7ed6a5f351260e81182e2e60924127b04f58af9f
Sunwoo Kim <ssunwoo0110@gmail.com>
2026-02-25 19:01:17 +0900

    [Gold V] Title: 회의실 배정, Time: 244 ms, Memory: 62436 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,25 @@
+import sys
+import heapq
+
+def main():
+    input = sys.stdin.readline
+    
+    n = int(input())
+    arr = []
+    
+    for _ in range(n):
+        arr.append(list(map(int, input().split())))
+    
+    arr.sort(key=lambda x: (x[1], x[0]))
+    
+    answer = 0
+    prev = -1
+    for s, e in arr:
+        if prev <= s:
+            answer += 1
+            prev = e
+    
+    print(answer)
+    
+if __name__ == "__main__":
+    main()
