ec1b557d8068690440fed2c70bc01394a0059e05
Sunwoo Kim <ssunwoo0110@gmail.com>
2026-03-03 17:26:13 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,23 @@
+import sys
+
+def main():
+    input = sys.stdin.readline
+    
+    n, m = map(int, input().split())
+    path = []
+    
+    def dfs(start, depth):
+        
+        if depth == m:
+            print(*path)
+            return
+        
+        for i in range(start, n+1):
+            path.append(i)
+            dfs(i+1, depth+1)
+            path.pop()
+    
+    dfs(1, 0)
+    
+if __name__ == "__main__":
+    main()
