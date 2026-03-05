8f53d99edbab5b0da295d1b4fa43db8719ec48c6
Sunwoo Kim <ssunwoo0110@gmail.com>
2026-03-05 18:59:03 +0900

    [Silver III] Title: 01타일, Time: 228 ms, Memory: 72404 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,20 @@
+import sys
+
+def main():
+    input = sys.stdin.readline
+    
+    MOD = 15746
+    
+    n = int(input())
+    
+    dp = [0 for _ in range(n+1)]
+    dp[0] = 1
+    dp[1] = 1
+    
+    for i in range(2, n+1):
+        dp[i] = (dp[i-1] + dp[i-2])%MOD
+    
+    print(dp[n])
+        
+if __name__ == "__main__":
+    main()
