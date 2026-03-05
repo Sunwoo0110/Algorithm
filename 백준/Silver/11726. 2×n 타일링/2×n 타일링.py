29d7184803cb8ddf77118a74780bb782be93d5fc
Sunwoo Kim <ssunwoo0110@gmail.com>
2026-03-05 18:52:37 +0900

    [Silver III] Title: 2×n 타일링, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,12 +1,20 @@
-num = int(input())
-DP = [0] * (1000+1)
-DP[0] = 0
-DP[1] = 1
-DP[2] = 2
+import sys
 
-def findAll(n):
-    for i in range(3, n+1):
-        DP[i] = DP[i-2] + DP[i-1]
-    return DP[n]
-
-print(findAll(num)%10007)
+def main():
+    input = sys.stdin.readline
+    
+    MOD = 10007
+    
+    n = int(input())
+    
+    dp = [0 for _ in range(n+1)]
+    dp[0] = 1
+    dp[1] = 1
+    
+    for i in range(2, n+1):
+        dp[i] = (dp[i-1] + dp[i-2])%MOD
+    
+    print(dp[n])
+        
+if __name__ == "__main__":
+    main()
--- 백준/Silver/11726. 2×n 타일링/README.md
+++ 백준/Silver/11726. 2×n 타일링/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 31256 KB, 시간: 44 ms
+메모리: 32412 KB, 시간: 36 ms
 
 ### 분류
 
@@ -12,7 +12,7 @@
 
 ### 제출 일자
 
-2023년 6월 16일 13:11:17
+2026년 3월 5일 18:51:08
 
 ### 문제 설명
 
@@ -20,7 +20,7 @@
 
 <p>아래 그림은 2×5 크기의 직사각형을 채운 한 가지 방법의 예이다.</p>
 
-<p style="text-align: center;"><img alt="" src="" style="height:50px; width:125px"></p>
+<p style="text-align: center;"><img alt="" src="https://onlinejudgeimages.s3-ap-northeast-1.amazonaws.com/problem/11726/1.png" style="height:50px; width:125px"></p>
 
 ### 입력 
 
