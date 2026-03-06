f4a9f3c1687985ea0d1ccfa8a4760c44841fce16
Sunwoo Kim <ssunwoo0110@gmail.com>
2026-03-06 21:32:53 +0900

    [Gold V] Title: 합분해, Time: 44 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,11 +1,27 @@
-n, k = map(int, input().split())
-DP = [[0] * (k+1) for _ in range(n+1)]
+import sys
 
-for i in range(k+1):
-    DP[0][i] = 1
-
-for i in range(0, n+1):
-    for j in range(1, k+1):
-        DP[i][j] = DP[i-1][j] + DP[i][j-1]
-
-print(DP[n][k] % 1000000000)
+def main():
+    input = sys.stdin.readline
+    
+    MOD = 1000000000
+    
+    n, k = map(int, input().split())
+    
+    ## dp[i][j] : j개 뽑아 합이 i가 되는 경우의 수
+    dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+    
+    for i in range(n+1):
+        dp[i][1] = 1
+    
+    for j in range(k+1):
+        dp[0][j] = 1
+    
+    for i in range(1, n+1):
+        for j in range(2, k+1):
+            dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % MOD
+    
+    print(dp[n][k])
+    
+    
+if __name__ == "__main__":
+    main()
